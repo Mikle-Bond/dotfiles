7315482633a9c48c21563131cf8a0edea635fe57
Mikle-Bond <Mikle_Bond@mail.ru>
2018-05-29 22:54:47 +0300

    [X] Put theme settings in separate script
    This is due to pinentry could not get this settings and looked ugly. Now
    it can include .xtheme.sh and look awesome (no).

---
+++ X/.xtheme.sh
@@ -0,0 +1,8 @@
+# Set GTK+ theme
+export GTK_THEME=Arc-Dark-solid
+export GTK2_RC_FILES=/usr/share/themes/$GTK_THEME/gtk-2.0/gtkrc 
+# Set Qt5 theme
+export QT_QPA_PLATFORMTHEME=gtk2
+export QT_STYLE_OVERRIDE=gtk2
+# NB: Qt4 style is set by qtconfig-qt4 in ~/.config/Trolltech.conf
+
