2ff088e0452a1ee7ca27037e5966e9f9d5aca386
Mikle-Bond <Mikle_Bond@mail.ru>
2018-05-30 17:03:39 +0300

    [i3] Added daemonization to start-workspace.sh
    This is to easily restart commonly-used windows.

--- i3/dev/scripts/start-workspace.sh
+++ i3/dev/scripts/start-workspace.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+DAEMONIZE=false
 declare -a PROGRAM
+
+if [ "$1" = '-d' ] ; then 
+	DAEMONIZE=true
+	shift
+fi
 
 case "$1" in
 	htop) PROGRAM=(urxvt -name Htop -e htop) ;;
@@ -8,5 +14,19 @@ case "$1" in
 	spad) PROGRAM=(urxvt -name urxvtq -g 166x21+15+30) ;;
 esac
 
-exec "${PROGRAM[@]}"
+function daemon() {
+	cd /
+	exec 0<&-
+	exec 1>/dev/null
+	exec 2>/dev/null
+
+	exec "$@"
+}
+
+if $DAEMONIZE ; then
+	daemon "${PROGRAM[@]}" &
+	disown
+else
+	exec "${PROGRAM[@]}"
+fi
 
