ffa613f7827a04e4ff4dfb8cff10ace143d1f257
Mikle-Bond <Mikle_Bond@mail.ru>
2018-05-31 00:03:24 +0300

    [bash] Added aliases for colorful output
    NOW TASTE'S LIKE RAINBOW ^o^

--- bash/.config/bashrc.d/glob/aliases.sh
+++ bash/.config/bashrc.d/glob/aliases.sh
@@ -8,11 +8,20 @@ alias say='trans -b -s en -speak -no-translate'
 alias mv='mv -i'
 alias rm='rm -i'
 
+# Color support
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+
 # Short-life pastebin
 alias tb="nc termbin.com 9999" 
 
-# TODO: Probably would need to shorten this
-alias hello='ping 8.8.8.8'
+# Shortenings
+alias ll='ls -la'
+alias hw='ping 8.8.8.8'
+alias hr="ping $(ip ro s default | awk '{print $3}')"
 
 alias tomoji="docker run -i --rm rsimmons/tomoji" 
 
