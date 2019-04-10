6b3bad2c6160b2acebaeb2d152f22a8005d14a8f
Mikle-Bond <Mikle_Bond@mail.ru>
2019-04-11 02:01:00 +0300

    [qutebrowser][bash] Separate qrshare functionality
    I've noticed, that it's a good shell script by it's own. Can share stuff
    through e.g. xclip.

--- qutebrowser/.local/share/qutebrowser/userscripts/qr-url.sh
+++ qutebrowser/.local/share/qutebrowser/userscripts/qr-url.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-file=$(mktemp --suffix='.png')
-trap "rm $file" 0 1 2 3 15
-
 if [ -z ${QUTE_URL+set} ] ; then 
 	QUTE_URL="$1"
 fi
 
-qrencode -s 15 -t PNG -o "$file" "$QUTE_URL"
-sxiv -s f $file
+qrshare "$QUTE_URL"
 
