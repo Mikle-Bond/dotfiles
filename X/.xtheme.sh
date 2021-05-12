0ae9aca6845563c6fbef3fcd2bde8d5e1eea2f5b
Mikle-Bond <Mikle_Bond@mail.ru>
2021-05-12 13:48:56 +0300

    [X] Try switching theme engine to qt5ct
    Apparently kdeconnect-app doesn't understand QT_STYLE_OVERRIDE

--- X/.xtheme.sh
+++ X/.xtheme.sh
@@ -3,8 +3,7 @@
 export GTK_THEME=wal
 export GTK2_RC_FILES="$HOME/.themes/$GTK_THEME/gtk-2.0/gtkrc"
 # Set Qt5 theme
-export QT_QPA_PLATFORMTHEME=gtk2
-export QT_STYLE_OVERRIDE=gtk2
+export QT_QPA_PLATFORMTHEME=qt5ct
 # NB: Qt4 style is set by qtconfig-qt4 in ~/.config/Trolltech.conf
 
 # inform system about our choice
